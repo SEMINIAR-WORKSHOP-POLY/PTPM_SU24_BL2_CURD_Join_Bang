4c9c0709e2113a4a765799fb0597a6dfb18270ad
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2024-07-16 20:07:50 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -32,8 +32,9 @@ CREATE TABLE [dbo].[NhanVien](
 	[DiaChi] [nvarchar](100) NULL,
 	[Sdt] [varchar](30) NULL,
 	[MatKhau] [varchar](max) NULL,
-	[IdCV] [int] NULL,
+	[IdCV] [int] NULL ,
 	[TrangThai] [int] NULL,
+    FOREIGN KEY (IdCV) REFERENCES ChucVu(ID),
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -50,7 +51,9 @@ SET IDENTITY_INSERT [dbo].[ChucVu] OFF
 
 SET IDENTITY_INSERT [dbo].[NhanVien] ON 
 
-INSERT [dbo].[NhanVien] ([Id], [Ma], [Ten], [TenDem], [Ho], [GioiTinh], [NgaySinh], [DiaChi], [Sdt], [MatKhau], [IdCV], [TrangThai]) VALUES (1, N'NV01', N'Hung', N'Quang', N'Le', N'Nam', CAST(N'2003-08-10' AS Date), N'Bac Ninh', N'0962823303',  1, NULL,  1)
-INSERT [dbo].[NhanVien] ([Id], [Ma], [Ten], [TenDem], [Ho], [GioiTinh], [NgaySinh], [DiaChi], [Sdt], [MatKhau], [IdCV], [TrangThai]) VALUES (2, N'NV02', N'Khai', N'Văn', N'Tran', N'Nữ', CAST(N'2002-09-02' AS Date), N'Lai Chau 98', N'0962823303',1, NULL, 1)
+INSERT [dbo].[NhanVien] ([Id], [Ma], [Ten], [TenDem], [Ho], [GioiTinh], [NgaySinh], [DiaChi], [Sdt], [MatKhau], [IdCV], [TrangThai]) VALUES (1, N'NV01', N'Hung', N'Quang', N'Le', N'Nam', CAST(N'2003-08-10' AS Date), N'Bac Ninh', N'0962823303',  1, 1,  1)
+INSERT [dbo].[NhanVien] ([Id], [Ma], [Ten], [TenDem], [Ho], [GioiTinh], [NgaySinh], [DiaChi], [Sdt], [MatKhau], [IdCV], [TrangThai]) VALUES (2, N'NV02', N'Khai', N'Văn', N'Tran', N'Nữ', CAST(N'2002-09-02' AS Date), N'Lai Chau 98', N'0962823303',1, 1, 1)
 SET IDENTITY_INSERT [dbo].[NhanVien] OFF
 
+SELECT * FROm NhanVien
+SELECT * FROM ChucVu
